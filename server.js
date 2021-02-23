38c5dfabdeeee1e825b7b48ce1e0921a41fe25cb
faiza203 <fiazfaiza981@gmail.com>
2021-02-23 12:59:36 -0500

    Update Video

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const { v4: uuidV4 } = require("uuid");
 const app = express();
-
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
-const port = process.env.PORT || 8000;
-const roomId = uuidV4();
-
+const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
-app.use(express.static("Public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.redirect(`/${roomId}`);
+  res.redirect(`/${uuidV4()}`);
 });
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
+
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
   });
 });
 
