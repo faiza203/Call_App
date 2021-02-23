375bc82c031d90fb13966e3daf2d764f01ebd8e0
faiza203 <fiazfaiza981@gmail.com>
2021-02-23 11:39:25 -0500

    Create Server with express

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { v4: uuidV4 } = require("uuid");
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const port = process.env.PORT || 8000;
+const roomId = uuidV4();
+
+app.set("view engine", "ejs");
+app.use(express.static("Public"));
+
+app.get("/", (req, res) => {
+  console.log(roomId);
+  res.redirect(`/${roomId}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.redirect("room", { roomId: req.params.room });
+});
+
+server.listen(port);
