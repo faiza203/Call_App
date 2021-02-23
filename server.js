21f83da4bcb361be275bde683afaac4f23ca5ef3
faiza203 <fiazfaiza981@gmail.com>
2021-02-23 13:33:52 -0500

    Update Video

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidV4 } = require("uuid");
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT | 8000;
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
